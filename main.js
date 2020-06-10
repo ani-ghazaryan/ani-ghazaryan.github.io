e5462facafc47c01a77c25e8d534e5f7d2e06236
anighazaryan <anighazaryan@workfront.com>
2020-06-10 12:24:51 +0400

    Draft implementation of Carousel

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ let buttons = document.querySelectorAll("button");
 buttons[0].addEventListener("click", function(){
     window.open(parent.location = './mycv.html');
 })
+
+buttons[1].addEventListener("click", function(){
+    window.open(parent.location = './carousel.html');
+})
